eb92cfc7f565f1f68aa2dd1a911aac7bc3269847
Spencer Marsh <scmarsh421@gmail.com>
2022-01-11 23:58:16 -0500

    Initial Commit

---
+++ FilmBuff.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FilmBuff.WebMVC.MvcApplication" Language="C#" %>
---
+++ FilmBuff.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FilmBuff.WebMVC.Startup))]
+namespace FilmBuff.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
